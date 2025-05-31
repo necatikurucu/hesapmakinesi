973b7be0e558aaf67c03c2655423f73a41a6066c
necati kurucu <kn_1907@hotmail.com>
2025-06-01 01:35:14 +0300

    Add project files.

---
+++ hesapmakinesi/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Channels;
+
+namespace hesapmakinesi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1, sayi2, sonuc;
+            char secim;
+            Console.WriteLine("1. sayı:");
+            sayi1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2. sayı:");
+            sayi2 = Convert.ToInt32(Console.ReadLine());
+                       
+            Console.Write(" + \n - \n * \n / \n seçiminiz(+,-,*,/)");
+            secim = Convert.ToChar(Console.ReadLine());
+            switch(secim)
+            {
+                case '+':
+                    sonuc = sayi1 + sayi2;
+                    Console.WriteLine("Sonuç :{0}", sonuc);
+                    break;
+                case '-':
+                    sonuc = sayi1 - sayi2;
+                    Console.WriteLine("Sonuç :{0}", sonuc);
+                    break;
+                case '*':
+                    sonuc = sayi1 * sayi2;
+                    Console.WriteLine("Sonuç :{0}", sonuc);
+                    break;
+                case '/':
+                    sonuc = sayi1 / sayi2;
+                    Console.WriteLine("Sonuç :{0}", sonuc);
+                    break;
+            }
+            
+
+           
+           
+
+
+
+
+                                   
+               
+        }
+
+    }
+}
